9e851717aaa325881ffee58761f8dcb9492776e8
315 <315@315NEW-10>
2021-04-29 15:36:20 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace CosmeticShop
+{
+    partial class Autorization
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Autorization));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.productBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.productBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(884, 70);
+            this.panel1.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(56, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(813, 21);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.DropDown);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.SearchText_KeyPress);
+            this.textBox1.KeyUp += new System.Windows.Forms.KeyEventHandler(this.SearchText_KeyUp);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 6);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Поиск";
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.Location = new System.Drawing.Point(0, 76);
+            this.panel2.MaximumSize = new System.Drawing.Size(880, 10000);
+            this.panel2.MinimumSize = new System.Drawing.Size(880, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(880, 454);
+            this.panel2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // productBindingSource
+            // 
+            this.productBindingSource.DataSource = typeof(CosmeticShop.Product);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 537);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // Autorization
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(225)))), ((int)(((byte)(228)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(900, 600);
+            this.MinimumSize = new System.Drawing.Size(900, 600);
+            this.Name = "Autorization";
+            this.Text = "Список товаров";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.productBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.BindingSource productBindingSource;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,255 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CosmeticShop
+{
+    public partial class Autorization : Form
+    {
+        DataBase db = new DataBase();
+
+        int counter;
+
+        public List<int> idList = new List<int>();
+
+        List<PictureBox> mainImage = new List<PictureBox>();
+
+        List<PictureBox> mainPanel = new List<PictureBox>();
+
+        List<Label> name = new List<Label>();
+
+        List<List<PictureBox>> checkList = new List<List<PictureBox>>();
+
+        List<List<string>> allImageList = new List<List<string>>();
+
+        List<string> allNameList = new List<string>();
+
+        List<int> idSearch = new List<int>();
+
+
+        public Autorization()
+        {
+            InitializeComponent();
+            FillIdlist();
+            CreatePanel(idList);
+
+        }
+
+        private void FillIdlist()
+        {
+            foreach (Product p in db.Product)
+            {
+                idList.Add(p.ID);
+                idSearch.Add(p.ID);
+                allNameList.Add(p.Title);
+            }
+        }
+
+        private void CreatePanel(List<int> id)
+        {
+            DeletePanel();
+            int count = id.Count;
+            int stringNum = 0;
+            for (int f = 0; f < count; f++)
+            {
+                counter = 1;
+                Product prd = db.Product.Find(id[f]);
+
+                List<string> imageList = new List<string>();
+                foreach (ProductPhoto pp in db.ProductPhoto)
+                {
+                    if (pp.ProductID == id[f])
+                        imageList.Add(pp.PhotoPath);
+                }
+                allImageList.Add(imageList);
+                counter += allImageList[f].Count;
+                    
+
+                if (f / (3 * (stringNum+1)) == 1)
+                {
+                    stringNum++;
+                }
+                PictureBox mainPn = new PictureBox()
+                {
+                    Size = new Size(200, 300),
+                    Location = new Point(100 + (250 * (f - 3*stringNum)), 50 + (Size.Height * stringNum/5*3)),
+                    BorderStyle = BorderStyle.FixedSingle,
+                    BackColor = Color.White,
+                    Name = $"{f}"
+                };
+                if (prd.IsActive == false)
+                {
+                    mainPn.BackColor = Color.LightGray;
+                }
+                
+                mainPanel.Add(mainPn);
+                panel2.Controls.Add(mainPanel[f]);
+
+                
+                Label text = new Label()
+                {
+                    Size = new Size(150, mainPanel[f].Height / 5),
+                    Location = new Point(mainPanel[f].Location.X + ((mainPanel[f].Width - 150) / 2), (mainPanel[f].Location.Y + mainPanel[f].Height) - mainPanel[f].Height / 5 - 10),
+                    Text = prd.Title,
+                    BackColor = mainPanel[f].BackColor,
+                    TextAlign = ContentAlignment.TopCenter,
+                    Font = new Font("Tahoma", 9f, FontStyle.Regular)
+                };
+                name.Add(text);
+                panel2.Controls.Add(name[f]);
+                name[f].BringToFront();
+
+                PictureBox mainImg = new PictureBox()
+                {
+                    Size = new Size(150, 200),
+                    BorderStyle = BorderStyle.FixedSingle,
+                    BackgroundImage = Image.FromFile($@"..\..\{prd.MainImagePath}"),
+                    BackgroundImageLayout = ImageLayout.Stretch,
+                    Location = new Point(mainPanel[f].Location.X + ((mainPanel[f].Width - 150) / 2), mainPanel[f].Location.Y + 10),
+                    Name = $"{f}"
+                };
+                mainImage.Add(mainImg);
+                panel2.Controls.Add(mainImage[f]);
+                mainImage[f].BringToFront();
+
+                List<PictureBox> pb = new List<PictureBox>();
+                for (int i = 0; i < counter; i++)
+                {
+                    PictureBox rdP = new PictureBox
+                    {
+                        Size = new Size(15, 15),
+                        BackColor = Color.FromArgb(225, 228, 255),
+                        Location = new Point((mainImage[f].Location.X + ((mainImage[f].Width - (30 * counter / 2)) / 2) + (30 * i) / 2), mainImage[f].Location.Y + mainImage[f].Height)
+                    };
+                    if (i == 0)
+                        rdP.BackColor = Color.FromArgb(255, 74, 109);
+                    pb.Add(rdP);
+                    panel2.Controls.Add(pb[i]);
+                    pb[i].BringToFront();
+                }
+                checkList.Add(pb);
+            }
+            label2.Text = $"{id.Count} из {idList.Count}";
+            HookUpEventHandlers();
+        }
+
+        void DeletePanel()
+        {
+            panel2.Controls.Clear();
+            name.Clear();
+            checkList.Clear();
+            mainPanel.Clear();
+            mainImage.Clear();
+            allImageList.Clear();
+        }
+
+        private void HookUpEventHandlers()
+        {
+            foreach (var p in mainImage)
+            {
+                p.MouseMove += P_MouseMove;
+                p.MouseLeave += P_MouseLeave;
+
+            }
+        }
+
+
+        private void P_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+            var pb = (PictureBox)sender;
+            Product prd = db.Product.Find(idSearch[Convert.ToInt32(pb.Name)]);
+            counter = 1 + allImageList[Convert.ToInt32(pb.Name)].Count;
+            double areaChange = pb.Width / counter;
+            for (int i = 0; i < counter; i++)
+            {
+                if (MousePosition.X >= this.Left + pb.Location.X + areaChange * i - 1
+                    && MousePosition.X <= this.Location.X + pb.Location.X + areaChange * (i + 1) + 1)
+                {
+                    if (i == 0)
+                    {
+                        pb.BackgroundImage = Image.FromFile($@"..\..\{prd.MainImagePath}");
+                    }
+                    else
+                    {
+                        pb.BackgroundImage = Image.FromFile($@"..\..\{allImageList[Convert.ToInt32(pb.Name)][i-1]}");
+                    }
+
+                    pb.BackgroundImageLayout = ImageLayout.Stretch;
+                    for (int j = 0; j < counter; j++)
+                    {
+                        if (j == i)
+                        {
+                            checkList[Convert.ToInt32(pb.Name)][j].BackColor = Color.FromArgb(255, 74, 109);
+                        }
+                        else
+                        {
+                            checkList[Convert.ToInt32(pb.Name)][j].BackColor = BackColor = Color.FromArgb(225, 228, 255);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void P_MouseLeave(object sender, EventArgs e)
+        {
+            var pb = (PictureBox)sender;
+            Product prd = db.Product.Find(idSearch[Convert.ToInt32(pb.Name)]);
+            mainImage[Convert.ToInt32(pb.Name)].BackgroundImage = Image.FromFile($@"..\..\{prd.MainImagePath}");
+            counter = 1 + allImageList[Convert.ToInt32(pb.Name)].Count;
+            for (int j = 0; j < counter; j++)
+            {
+                if (j == 0)
+                {
+                    checkList[Convert.ToInt32(pb.Name)][j].BackColor = Color.FromArgb(255, 74, 109);
+                }
+                else
+                {
+                    checkList[Convert.ToInt32(pb.Name)][j].BackColor = BackColor = Color.FromArgb(225, 228, 255);
+                }
+            }
+        }
+
+
+      
+
+        private void SearchText_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+        }
+
+        private void DropDown(object sender, EventArgs e)
+        {
+            idSearch.Clear();
+            for (int i = 0; i < idList.Count(); i++)
+            {
+                if (allNameList[i].Contains($"{textBox1.Text}") == true)
+                {
+                        idSearch.Add(idList[i]);
+                }
+            }
+            CreatePanel(idSearch);
+            
+            
+            
+            
+        }
+
+        private void SearchText_KeyUp(object sender, KeyEventArgs e)
+        {
+            
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
